078b16bebf9b1060397b694b2140ee7ab3fd6934
tanzhijian <tanzhijian@surfilter.com>
2022-05-25 17:20:38 +0800

    更改指令权重

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ export { getRandomApply, randomApply, addRandomApplyContext };
 let rule = {
   getRandomApply: {
     reg: "noCheck",
-    priority: 9000,
+    priority: 50,
     describe: "回复添加的随机回复",
   },
   // 覆盖默认的添加表情行为
   randomApply: {
     reg: "^#*添加(.*)",
-    priority: 9001,
+    priority: 51,
     describe: "【添加哈哈】添加内容",
   },
   addRandomApplyContext: {
     reg: "noCheck",
-    priority: 9002,
+    priority: 52,
     describe: "添加随机回复上下文",
   },
 };
